63292c0d3fe31c8fbcb0980e30ce2e9f58056fdd
Aslan Simaiev <72418039+aslansimaiev@users.noreply.github.com>
2025-08-22 11:45:50 +0300

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by NIL on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
