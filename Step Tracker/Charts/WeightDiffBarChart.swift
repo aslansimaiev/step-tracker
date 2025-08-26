f0f7f5bcded0239b8b4ad2e3df9d3d166c2f22e7
Aslan Simaiev <72418039+aslansimaiev@users.noreply.github.com>
2025-08-26 22:24:40 +0300

    implement chart empty state

--- Step Tracker/Charts/WeightDiffBarChart.swift
+++ Step Tracker/Charts/WeightDiffBarChart.swift
@@ -12,7 +12,7 @@ struct WeightDiffBarChart: View {
     
     @State private var rawSelectedDate: Date?
     @State private var selectedDay: Date?
-
+    
     var chartData: [WeekdayChartData]
     
     var selectedData: WeekdayChartData? {
@@ -40,39 +40,44 @@ struct WeightDiffBarChart: View {
             
             .foregroundStyle(.secondary)
             .padding(.bottom, 12)
-            Chart {
-                if let selectedData {
-                    RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
-                        .foregroundStyle(Color.secondary.opacity(0.3))
-                        .offset(y: -10)
-                        .annotation(position: .top,
-                                    spacing: 0,
-                                    overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
-                            annotationView
-                        }
+            if chartData.isEmpty {
+                ChartEmptyView(systemImageName: "chart.bar", title: "No Data", description: "There is no weight data from the Health App.")
+            } else {
+                Chart {
+                    if let selectedData {
+                        RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
+                            .foregroundStyle(Color.secondary.opacity(0.3))
+                            .offset(y: -10)
+                            .annotation(position: .top,
+                                        spacing: 0,
+                                        overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
+                                annotationView
+                            }
+                    }
+                    ForEach(chartData) { weightDiff in
+                        BarMark(x: .value("Date", weightDiff.date, unit: .day),
+                                y: .value("Weight Diff", weightDiff.value)
+                        )
+                        .foregroundStyle(weightDiff.value >= 0 ? Color.indigo.gradient : Color.mint.gradient)
+                    }
                 }
-                ForEach(chartData) { weightDiff in
-                    BarMark(x: .value("Date", weightDiff.date, unit: .day),
-                            y: .value("Weight Diff", weightDiff.value)
-                    )
-                    .foregroundStyle(weightDiff.value >= 0 ? Color.indigo.gradient : Color.mint.gradient)
+                .frame(height: 150)
+                .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day)) {
+                        AxisValueLabel(format: .dateTime.weekday(), centered: true)
+                    }
+                }
+                .chartYAxis {
+                    AxisMarks { value in
+                        AxisGridLine()
+                            .foregroundStyle(Color.secondary.opacity(0.3))
+                        
+                        AxisValueLabel()
+                    }
                 }
             }
-            .frame(height: 150)
-            .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
-            .chartXAxis {
-                AxisMarks(values: .stride(by: .day)) {
-                    AxisValueLabel(format: .dateTime.weekday(), centered: true)
-                }
-            }
-            .chartYAxis {
-                AxisMarks { value in
-                    AxisGridLine()
-                        .foregroundStyle(Color.secondary.opacity(0.3))
-                    
-                    AxisValueLabel()
-                }
-            }
+            
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
@@ -83,7 +88,7 @@ struct WeightDiffBarChart: View {
             }
         }
     }
-     
+    
     var annotationView: some View {
         VStack (alignment: .leading){
             Text(selectedData?.date ?? .now, format: .dateTime.weekday(.abbreviated).month().day())
